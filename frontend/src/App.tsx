565ed4fd2a1a4b2cdd0b50ce1db20ee8e0f78310
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2025-11-02 20:04:25 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./pages/Navbar/Navbar"
 import EventList from "./pages/EventList/Eventlist"
